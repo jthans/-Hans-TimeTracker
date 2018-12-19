da743b8e9df7c78a14164ad5f53873189f67f3a9
Taylor Hans <THans@inspirewellness.onmicrosoft.com>
2018-12-19 16:02:02 -0500

    Adding Test Project.

---
+++ Tests/Hans.App.TimeTracker.Test/Controllers/SlackControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hans.App.TimeTracker.Test.Controllers
+{
+    /// <summary>
+    ///  Ensures all endpoints on the <see cref="SlackController" /> are valid, and behave as expected for the design.
+    /// </summary>
+    [TestClass]
+    public class SlackControllerTest
+    {
+        /// <summary>
+        ///  Ensures the echo endpoint successfully returns the expected result.
+        /// </summary>
+        [TestMethod]
+        public void Echo_ReturnsSuccess()
+        {
+
+        }
+    }
+}
